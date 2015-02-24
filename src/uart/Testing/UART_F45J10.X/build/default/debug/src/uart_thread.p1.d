162f57c806e44228a6543c81fdab8c115addbf9e
cspang1 <cspang1@vt.edu>
2015-02-24 09:09:37 -0800

    Updated PIC18F45J10 testbench to start and stop motors at interval through interrupt/message driven EUSART

---
+++ src/uart/Testing/UART_F45J10.X/build/default/debug/src/uart_thread.p1.d
@@ -0,0 +1,6 @@
+ build/default/debug/src/uart_thread.d  \
+ build/default/debug/src/uart_thread.p1:  \
+ src/uart_thread.c  \
+src/maindefs.h  \
+src/messages.h  \
+src/uart_thread.h 
--- src/uart/Testing/UART_F45J10.X/src/timer1_thread.h
+++ src/uart/Testing/UART_F45J10.X/src/timer1_thread.h
@@ -1,6 +1,8 @@
 typedef struct __timer1_thread_struct {
     // "persistent" data for this "lthread" would go here
     unsigned int msgcount;
+    unsigned char even;
+    unsigned char buffer[10];
 } timer1_thread_struct;
 
 void init_timer1_lthread(timer1_thread_struct *);
